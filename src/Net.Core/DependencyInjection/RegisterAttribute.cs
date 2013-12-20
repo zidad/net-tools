38e7543aa688cddf5af97dc596c1a0c500e12d6b
Wiebe Tijsma <wiebe@tijsma.com>
2013-12-20 15:32:20 +0100

    added DependencyInjection attributes

---
+++ src/Net.Core/DependencyInjection/RegisterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Net.Annotations;
+
+namespace Net.DependencyInjection
+{
+    /// <summary>
+    /// mark a class or interface as a service to be automatically discovered
+    /// </summary>
+    [MeansImplicitUse]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+    public class RegisterAttribute : Attribute
+    {
+        public RegisterAttribute()
+        {
+            IsDefaultImplementation = false;
+            Ignore = false;
+        }
+
+        public int Order { get; set; }
+
+        public bool IsDefaultImplementation { get; set; }
+        public bool Ignore { get; set; }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string LifeTimeScopeTag { get; set; }
+    }
+}
