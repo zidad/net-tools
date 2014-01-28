a4453a1c76ff2cc83c9dd5db4f8d310f7269debf
wtijsma <wiebe@tijsma.com>
2014-01-28 12:49:37 +0100

    bumped version

--- src/Version.cs
+++ src/Version.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.1")]
 
 [assembly: AssemblyCompany("NetIndustry.nl, Codeworks.nl, Shader.nl")]
 [assembly: AssemblyProduct("Net.Tools")]
