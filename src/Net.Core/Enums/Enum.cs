253d34f59086d7934a21224969c69c30c35ff839
wtijsma <wiebe@tijsma.com>
2014-05-06 20:13:34 +0200

    added Enum helper classes

---
+++ src/Net.Core/Enums/Enum.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Net.Reflection;
+using Net.System;
+
+namespace Net.Enums
+{
+    public class Enum
+    {
+        public static TAttribute GetAttribute<TAttribute>(object enumValue)
+            where TAttribute : Attribute
+        {
+            if (enumValue == null) return null;
+
+            var members = enumValue.GetType().GetMember(enumValue.ToString());
+
+            if (members.Length == 0) return null;
+
+            return members[0].GetCustomAttribute<TAttribute>();
+        }
+
+        public static string GetShortName<TEnum>(TEnum? @enum) where TEnum : struct
+        {
+            if (@enum == null) return string.Empty;
+
+            return GetShortName(@enum.Value);
+        }
+
+        public static string GetName<TEnum>(TEnum @enum) where TEnum : struct
+        {
+            return GetAttribute<DisplayAttribute>(@enum).Get(s => s.Name, string.Empty);
+        }
+
+        public static string GetShortName<TEnum>(TEnum @enum) where TEnum : struct
+        {
+            return GetAttribute<DisplayAttribute>(@enum).Get(s => s.ShortName, string.Empty);
+        }
+
+        public static string GetDescription<TEnum>(TEnum? @enum) where TEnum : struct
+        {
+            return GetAttribute<DisplayAttribute>(@enum).Get(s => s.Description);
+        }
+
+        public static string GetDescription<TEnum>(TEnum @enum) where TEnum : struct
+        {
+            return GetAttribute<DisplayAttribute>(@enum).Get(s => s.Description, string.Empty);
+        }
+
+        public static global::System.Enum ByShortName(Type enumType, string shortName)
+        {
+            var enumValue = (global::System.Enum)enumType.GetFields()
+                .Where(e => e.GetAttribute<DisplayAttribute>().Get(s => s.ShortName) == shortName)
+                .Select(c => c.GetValue(null))
+                .FirstOrDefault();
+
+            return enumValue;
+        }
+
+        public static TEnum Parse<TEnum>(string enumValue)
+            where TEnum : struct
+        {
+            TEnum e;
+
+            if (global::System.Enum.TryParse(enumValue, true, out e))
+                return e;
+
+            return default(TEnum);
+        }
+    }
+
+    [Serializable]
+    public class Enum<TEnum>
+        where TEnum : struct
+    {
+        private readonly int value;
+        private readonly string name;
+        private readonly TEnum enumValue;
+
+        public Enum(TEnum value)
+        {
+            enumValue = value;
+            this.value = Convert.ToInt32(value);
+            name = value.ToString();
+        }
+
+        public T GetAttribute<T>() where T : Attribute
+        {
+            return typeof(TEnum).GetMember(name)[0].GetCustomAttribute<T>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Value
+        {
+            get { return value; }
+        }
+
+        public TEnum EnumValue
+        {
+            get { return enumValue; }
+        }
+
+        public static TEnum Parse(string name, bool ignoreCase = false)
+        {
+            return (TEnum)global::System.Enum.Parse(typeof(TEnum), name, ignoreCase);
+        }
+
+        public static bool TryParse(string name, out TEnum enumvalue)
+        {
+            EnumList<TEnum> list = EnumList<TEnum>.Instance;
+            if (!list.Contains(name))
+            {
+                enumvalue = default(TEnum);
+                return false;
+            }
+            enumvalue = list[name].EnumValue;
+            return true;
+        }
+
+        public static IEnumerable<TEnum> GetValues()
+        {
+            return global::System.Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+        }
+
+        public static TEnum ConvertFrom<T>(T other)
+        {
+            return Parse(other.ToString(), true);
+        }
+
+        public static TEnum? ByShortName(string code)
+        {
+            return ByAttribute<DisplayAttribute>(attribute => attribute != null && attribute.ShortName == code);
+        }
+
+        public static TEnum? ByAttribute<TAttribute>(Func<TAttribute, bool> predicate) where TAttribute : Attribute
+        {
+            return EnumList<TEnum>.Instance.FirstOrDefault(v => predicate(v.GetAttribute<TAttribute>())).Get(e => (TEnum?)e.EnumValue);
+        }
+
+        public static bool IsDefined(int i)
+        {
+            return global::System.Enum.IsDefined(typeof(TEnum), i);
+        }
+    }
+}
---
+++ src/Net.Core/Enums/EnumList.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Net.Enums
+{
+    /// <summary>
+    /// a bindable, double-indexed hashlist for any enum type
+    /// </summary>
+    [Serializable]
+    public sealed class EnumList<TEnum> : KeyedCollection<string, Enum<TEnum>>
+        where TEnum : struct
+    {
+        private static volatile EnumList<TEnum> defaultInstance;
+        
+        // ReSharper disable once StaticFieldInGenericType
+        private static readonly object LockObj = new object();
+
+        public static EnumList<TEnum> Instance
+        {
+            get
+            {
+                if (defaultInstance != null)
+                    return defaultInstance;
+
+                lock (LockObj)
+                    if (defaultInstance == null)
+                        defaultInstance = new EnumList<TEnum>(true);
+
+                return defaultInstance;
+            }
+        }
+
+        public EnumList(bool autoFill = false)
+        {
+            if (autoFill)
+                FillList(typeof(TEnum));
+        }
+
+        public Enum<TEnum> this[TEnum enumValue]
+        {
+            get
+            {
+                return this.First(o => o.EnumValue.Equals(enumValue));
+            }
+        }
+
+        public void Add(TEnum value)
+        {
+            Add(CreateEntry(value));
+        }
+
+        public void Add(params TEnum[] value)
+        {
+            foreach (var tEnum in value)
+            {
+                Add(CreateEntry(tEnum));
+            }
+        }
+
+        public void Remove(TEnum value)
+        {
+            Remove(this[value]);
+        }
+
+        public static EnumList<T> Fill<T>(T enumType)
+            where T : struct
+        {
+            return new EnumList<T>(true);
+        }
+
+        private void FillList(Type enumType)
+        {
+            Clear();
+            var valuesArray = global::System.Enum.GetValues(enumType);
+            var entryCount = valuesArray.Length;
+            var values = new TEnum[entryCount];
+            valuesArray.CopyTo(values, 0);
+            for (var i = 0; i < entryCount; i++)
+            {
+                Add(CreateEntry(values[i]));
+            }
+        }
+
+        private static Enum<TEnum> CreateEntry(TEnum value)
+        {
+            return new Enum<TEnum>(value);
+        }
+
+        protected override string GetKeyForItem(Enum<TEnum> item)
+        {
+            return item.Name;
+        }
+    }
+}
