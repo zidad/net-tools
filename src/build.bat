bb6f84b3ebce273b8e12bb1ebf9e85d8af177bcc
wtijsma <wiebe@tijsma.com>
2014-05-09 12:32:58 +0200

    upped version

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,3 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" ".build\Net.Tools.proj"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" ".build\Net.Tools.proj" /p:VisualStudioVersion=12.0
 
 pause
