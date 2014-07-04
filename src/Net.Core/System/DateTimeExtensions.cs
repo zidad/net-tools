013e5258f7ca823ed614276bb46c37a526f0bb75
wtijsma <wiebe@tijsma.com>
2014-07-05 00:36:20 +0200

    added more DateTimeExtensions.Between overloads

--- src/Net.Core/System/DateTimeExtensions.cs
+++ src/Net.Core/System/DateTimeExtensions.cs
@@ -16,5 +16,13 @@ namespace Net.System
             
             return date <= end && date >= start;
         }
+
+        public static bool Between(this DateTime? date, DateTime start, DateTime end)
+        {
+            if(!date.HasValue)
+                return false;
+            
+            return date <= end && date >= start;
+        }
     }
 }
