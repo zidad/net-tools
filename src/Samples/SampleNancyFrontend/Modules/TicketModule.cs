6fbadef86b29f82f752eaf4c10a68c1ba557ce1e
wtijsma <wiebe@tijsma.com>
2014-10-21 12:46:20 +0200

    updated all packages

---
+++ src/Samples/SampleNancyFrontend/Modules/TicketModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace SampleMessages.Modules
+{
+    public class TicketModule : NancyModule
+    {
+        public TicketModule()
+            : base("tickets")
+        {
+
+            Get["/"] = _ => Response.AsRedirect("~/tickets");
+
+            Get["/tickets"] = _ => View["reserve"];
+        }
+    }
+}
