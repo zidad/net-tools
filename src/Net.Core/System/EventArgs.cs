6f8b8a56ae437d8cf52bdbc80698e919ac2f39c9
wtijsma <wiebe@tijsma.com>
2015-04-15 16:37:12 +0200

    added generic EventArgs class

---
+++ src/Net.Core/System/EventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Net.System
+{
+    /// <summary>
+    /// An generic event args that can give back an extra typed value
+    /// </summary>
+    /// <typeparam name="T">The type of the value that should be given back</typeparam>
+    public class EventArgs<T> : EventArgs
+    {
+        private readonly T value;
+
+        public EventArgs(T value)
+        {
+            if (Equals(value, null))
+                throw new Exception("Value of argument is not set");
+
+            this.value = value;
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+    }
+}
