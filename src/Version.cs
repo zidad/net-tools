d1407c9a3e50a14f4a5eef56951de2a0c2556f91
wtijsma <wiebe@tijsma.com>
2016-10-26 16:46:22 +0300

    Updated FluentMigrator

--- src/Version.cs
+++ src/Version.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.12.0")]
-[assembly: AssemblyFileVersion("0.1.12.0")]
+[assembly: AssemblyVersion("0.1.13.0")]
+[assembly: AssemblyFileVersion("0.1.13.0")]
 
 [assembly: AssemblyCompany("NetIndustry.nl, Codeworks.nl, Shader.nl")]
 [assembly: AssemblyProduct("Net.Tools")]
-[assembly: AssemblyCopyright("MIT Licensed, copyright ©  2014")]
+[assembly: AssemblyCopyright("MIT Licensed, copyright ©  2016")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
